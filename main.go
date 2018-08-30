1b56b92e232c7f075ec4eac36386f0ff07619307
Jesse Epperson <Jepperson@remitly.com>
2018-08-30 02:57:46 -0700

    Fetch AWS config from ec2metadata if on an ec2 instance (#5)
    * Testing chain provider for credentials
    
    * Used metaSession to supply region from ec2metadata when applicable
    
    * Uncommented build platforms
    
    * Updated provisioner to fall back to region from ec2metadata
    
    * Don't ignore errors

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
-	"github.com/aws/aws-sdk-go/aws"
 )
 
 func RetrieveSecret(variableName string) {
@@ -17,8 +18,29 @@ func RetrieveSecret(variableName string) {
 	sess, err := session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	})
-	if err != nil { // resp is now filled
+	if err != nil {
 		printAndExit(err)
+	}
+
+	// AWS Go SDK does not currently support automatic fetching of region from ec2metadata.
+	// If the region could not be found in an environment variable or a shared config file,
+	// create metaSession to fetch the ec2 instance region and pass to the regular Session.
+	if *sess.Config.Region == "" {
+		metaSession, err := session.NewSession()
+		if err != nil {
+			printAndExit(err)
+		}
+
+		metaClient := ec2metadata.New(metaSession)
+		// If running on an EC2 instance, the metaClient will be available and we can set the region to match the instance
+		// If not on an EC2 instance, the region will remain blank and AWS returns a "MissingRegion: ..." error
+		if metaClient.Available() {
+			if region, err := metaClient.Region(); err == nil {
+				sess.Config.Region = aws.String(region)
+			} else {
+				printAndExit(err)
+			}
+		}
 	}
 
 	// Create a new instance of the service's client with a Session.
